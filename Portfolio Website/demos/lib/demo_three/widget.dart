edb9690febd593af63ad43058a29b34db5aa874b
abdullahth <abdullahthurwatx@gmail.com>
2021-08-07 14:29:46 +0200

    Demo Three Init

---
+++ Portfolio Website/demos/lib/demo_three/widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DemoThree extends StatefulWidget {
+  const DemoThree({Key? key}) : super(key: key);
+
+  @override
+  _DemoThreeState createState() => _DemoThreeState();
+}
+
+class _DemoThreeState extends State<DemoThree> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
