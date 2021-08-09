7eef60dca21772f846f4e5d7cdaca997e801b8de
abdullahth <abdullahthurwatx@gmail.com>
2021-08-09 08:49:57 +0200

    Router init

---
+++ Portfolio Website/production/lib/preferences/router.dart
@@ -0,0 +1,12 @@
+import 'package:fluro/fluro.dart';
+
+class AppRouter {
+  static FluroRouter router = FluroRouter();
+  static void setupRouter() {
+    // router.define(
+    //   '/',
+    //   handler: Handler(handlerFunc: (_, __) => DemoThree()),
+    // );
+   
+  }
+}
