6d24774745e9b3bcef04922b07e81014bd986b60
abdullahth <abdullahthurwatx@gmail.com>
2021-08-10 14:11:00 +0200

    Updated

--- Portfolio Website/production/lib/preferences/consts.dart
+++ Portfolio Website/production/lib/preferences/consts.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
-final lightTheme = ThemeData();
+final lightTheme = ThemeData(
+  scaffoldBackgroundColor: Color(0xFFF9F9F9),
+  primaryColor: Color(0xFF00ff5a),
+  accentColor: Color(0xFF458bff),
+);
 
-final darkTheme = ThemeData();
+final darkTheme = ThemeData(
+  scaffoldBackgroundColor: Color(0xFF262626),
+  primaryColor: Color(0xFF00ff5a),
+  accentColor: Color(0xFFffffff),
+);
