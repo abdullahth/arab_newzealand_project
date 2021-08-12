f1805ef6e7bb811032e2b13796bf71e1b31c169a
abdullahth <abdullahthurwatx@gmail.com>
2021-08-12 07:22:30 +0200

    Fade In Transition

---
+++ Portfolio Website/production/lib/transitions/fade_in.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class FadeInTransition extends StatefulWidget {
+  Widget child;
+  FadeInTransition(this.child);
+  @override
+  _FadeInTransitionState createState() => _FadeInTransitionState();
+}
+
+class _FadeInTransitionState extends State<FadeInTransition>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    _controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    _animation = Tween<double>(begin: 0, end: 0.5).animate(_controller);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: _animation,
+      child: widget.child,
+    );
+  }
+}
