e3892e4e686cc0434f544a4347ac10f094b1858b
abdullahth <abdullahthurwatx@gmail.com>
2021-08-12 10:47:34 +0200

    Companies Page Init

---
+++ Portfolio Website/production/lib/screens/companies_page/companies_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CompaniesPage extends StatefulWidget {
+  const CompaniesPage({Key? key}) : super(key: key);
+
+  @override
+  _CompaniesPageState createState() => _CompaniesPageState();
+}
+
+class _CompaniesPageState extends State<CompaniesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
