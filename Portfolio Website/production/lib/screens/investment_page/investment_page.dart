c68dcd16c7ed202d85e6b33ac846edcdf8c27b83
abdullahth <abdullahthurwatx@gmail.com>
2021-08-12 10:47:04 +0200

    Investment Page Init

---
+++ Portfolio Website/production/lib/screens/investment_page/investment_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class InvestmentPage extends StatefulWidget {
+  const InvestmentPage({ Key? key }) : super(key: key);
+
+  @override
+  _InvestmentPageState createState() => _InvestmentPageState();
+}
+
+class _InvestmentPageState extends State<InvestmentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
