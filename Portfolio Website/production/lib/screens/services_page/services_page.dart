cd8fb172508f85c36394b5ed6cd9b85399bcd16c
abdullahth <abdullahthurwatx@gmail.com>
2021-08-12 10:47:59 +0200

    Services Page Init

---
+++ Portfolio Website/production/lib/screens/services_page/services_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Servicespage extends StatefulWidget {
+  const Servicespage({Key? key}) : super(key: key);
+
+  @override
+  _ServicespageState createState() => _ServicespageState();
+}
+
+class _ServicespageState extends State<Servicespage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
