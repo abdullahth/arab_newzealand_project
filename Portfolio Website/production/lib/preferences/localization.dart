e3f3a7c98bb09f3313be8b39a4515d7871752dc9
abdullahth <abdullahthurwatx@gmail.com>
2021-08-19 13:11:17 +0200

    18/9

--- Portfolio Website/production/lib/preferences/localization.dart
+++ Portfolio Website/production/lib/preferences/localization.dart
@@ -24,6 +24,15 @@ class Messages extends Translations {
           // Journey Page
           'tour_message': "تأكد من إغتنام الرحلة إلى نيوزيلندا!",
           'discover': "إكتشف...",
+          'where_to_go': "أين تذهب",
+          'where_to_stay': "أين تبيت",
+          'where_to_eat': "أين تأكل",
+          'what_services': "أي خدمات",
+          'will_you_need': "سوف تحتاج لها",
+          'in': "في",
+          'categories': "التصنيفات",
+          'cities': "المدن",
+          'hotels': "الفنادق",
 
           // Categories
           'health': "الصحة",
@@ -36,10 +45,18 @@ class Messages extends Translations {
           'tourism': "السياحة",
           'landscape': "المعالم الطبيعية",
 
-          // Journey Slides
-          'slide_one': "أبطال الراجبي يحققون التاريخ!",
-          'slide_two': "أنت تعرف عن مزارعنا!",
-          'slide_three': "نتحداك أن تجد ماهو خلاب أكثر من هذا!",
+          // Cities
+          'auckland': "مدينة أوكلاند",
+          'auckland_brief': "عاصمة الجنة النيوزلاندية",
+          'hamilton': "مدينة هاميلتون",
+          'hamilton_brief':
+              "هي مدينة في الجزيرة الشمالية لنيوزيلندا. تقع على ضفاف نهر وايكاتو ، وهي والأكثر اكتظاظًا بالسكان في منطقة وايكاتو.",
+          'christchurch': "مدينة كرايستشيرش",
+          'christchurch_brief':
+              "هي أكبر مدينة في الجزيرة الجنوبية لنيوزيلندا ومقر منطقة كانتربري وتقع مدينة كرايستشيرش على الساحل الشرقي للجزيرة الجنوبية ، شمال شبه جزيرة بانكس على خليج بيغاسوس.",
+          'welington': "مدينة ويلنجتون",
+          'welington_brief':
+              "تقع في الطرف الجنوبي الغربي من الجزيرة الشمالية ، بين مضيق كوك وسلسلة جبال ريموتاكا وهي المركز السكاني الرئيسي للجزيرة الشمالية الجنوبية.",
         },
         "en": {
           // Home Page
@@ -62,6 +79,15 @@ class Messages extends Translations {
           // Journey Page
           'tour_message': "Be sure to embrace the journey to New Zealand!",
           'discover': "Discover...",
+          'where_to_go': "Where to go",
+          'where_to_stay': "Where to stay",
+          'where_to_eat': "Where to eat",
+          'what_services': "What Services",
+          'will_you_need': "will you need",
+          'in': "in",
+          'categories': "Categories",
+          'cities': "Cities",
+          'hotels': "Hotels",
 
           // Categories
           'health': "Health",
@@ -74,10 +100,18 @@ class Messages extends Translations {
           'tourism': "Tourism",
           'landscape': "Landscapes",
 
-          // Journey Slides
-          'slide_one': "Rugby Boys Keep it real!",
-          'slide_two': "You know about our farms!",
-          'slide_three': "Challenge You to find somewhere more bewitching!",
+          // Cities
+          'auckland': "Auckland City",
+          'auckland_brief': "Capital of New Zealand Heaven",
+          'hamilton': "Hamilton City",
+          'hamilton_brief':
+              "is a city in the North Island of New Zealand. Located on the banks of the Waikato River, it is the seat and most populous city of the Waikato region.",
+          'christchurch': "Christchurch City",
+          'christchurch_brief':
+              "is the largest city in the South Island of New Zealand and the seat of the Canterbury Region and lies on the South Island's east coast, just north of Banks Peninsula on Pegasus Bay.",
+          'welington': "Wellington City",
+          'welington_brief':
+              "It is located at the south-western tip of the North Island, between Cook Strait and the Remutaka Range and it's the major population centre of the southern North Island",
         },
       };
 }
