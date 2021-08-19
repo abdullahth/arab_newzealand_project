c5998d113669591559f3694a855b2355b8c661ac
abdullahth <abdullahthurwatx@gmail.com>
2021-08-19 13:19:40 +0200

    Mobile Body init

---
+++ Portfolio Website/production/lib/screens/journey_page/componets/devices_body/mobile_body/mobile_body.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:production/preferences/dims_config.dart';
+
+class MobileJourneyPage extends StatefulWidget {
+  @override
+  MobileJourneyPageState createState() => MobileJourneyPageState();
+}
+
+class MobileJourneyPageState extends State<MobileJourneyPage> {
+  @override
+  Widget build(BuildContext context) {
+    final dims = DimsConfig(context);
+    final theme = Theme.of(context);
+
+    return Scaffold();
+  }
+}
