db23b793c3e0be88c11b4ed6b12104a190908530
abdullahth <abdullahthurwatx@gmail.com>
2021-08-20 14:01:38 +0200

    20/8/21

--- Portfolio Website/production/lib/preferences/localization.dart
+++ Portfolio Website/production/lib/preferences/localization.dart
@@ -4,6 +4,7 @@ class Messages extends Translations {
   @override
   Map<String, Map<String, String>> get keys => {
         "ar": {
+          'question_mark': "؟",
           // Home Page
           'welcome': "أهلاً بك في...",
           'newzealand': "نيوزيلاندا",
@@ -33,6 +34,7 @@ class Messages extends Translations {
           'categories': "التصنيفات",
           'cities': "المدن",
           'hotels': "الفنادق",
+          'per_night': "في الليلة",
 
           // Categories
           'health': "الصحة",
@@ -57,8 +59,28 @@ class Messages extends Translations {
           'welington': "مدينة ويلنجتون",
           'welington_brief':
               "تقع في الطرف الجنوبي الغربي من الجزيرة الشمالية ، بين مضيق كوك وسلسلة جبال ريموتاكا وهي المركز السكاني الرئيسي للجزيرة الشمالية الجنوبية.",
+
+          // Hotels
+          'cordis_hotel': "فندق كورديس،أوكلاند",
+          'cordis_brief':
+              "فندق كورديس ، أوكلاند هو فندق راقٍ في أوكلاند ، نيوزيلندا. كان يُعرف سابقًا باسم The Langham ، أوكلاند ، ويحتل الموقع التاريخي لـ Partington's Windmill ، وهو معلم محلي حتى تم هدمه في عام 1950.",
+          'hilton_hotel': "فندق هيلتون، أوكلاند",
+          'hilton_brief':
+              "فنادق ومنتجعات هيلتون هي علامة تجارية عالمية للفنادق والمنتجعات كاملة الخدمات والعلامة التجارية الرائدة لشركة الضيافة الأمريكية متعددة الجنسيات هيلتون.",
+          'novotel_hotel': "فندق نوفوتيل",
+          'novotel_brief':
+              "نوفوتيل علامة تجارية فندقية متوسطة الحجم مملوكة لشركة أكور. تأسست في عام 1967 في فرنسا ، ونمت الشركة لتصبح مجموعة Accor في عام 1983 ، وظلت نوفوتيل علامة تجارية أساسية لاستراتيجية Accor متعددة العلامات التجارية. تدير نوفوتيل 492 فندقًا في 59 دولة.",
+          'sofitel_hotel': "فندق سوفيتيل",
+          'sofitel_brief':
+              "فنادق ومنتجعات سوفيتيل هي سلسلة من الفنادق الفاخرة مقرها في باريس ، فرنسا ، وتملكها شركة أكور منذ عام 1980.",
+
+          // Quoters
+          'evangline_quote':
+              "كانت الأجزاء المفضلة لدي في \"معركة الجيوش الخمسة\" هي اللحظات التي يمكنك أن ترى فيها بوضوح أننا ننظر إلى نيوزيلندا. لم يتم ذلك بعد ، لم يكن CGI ، لقد كان الخلق الجميل والمدهش للطبيعة الأم في كل روعتها.",
+          'evans_quote': "أشعر أن نيوزيلندا هي بيتي الثاني."
         },
         "en": {
+          'question_mark': "?",
           // Home Page
           'welcome': "Welcome to...",
           'newzealand': "New Zealand",
@@ -88,6 +110,7 @@ class Messages extends Translations {
           'categories': "Categories",
           'cities': "Cities",
           'hotels': "Hotels",
+          'per_night': "/N",
 
           // Categories
           'health': "Health",
@@ -112,6 +135,25 @@ class Messages extends Translations {
           'welington': "Wellington City",
           'welington_brief':
               "It is located at the south-western tip of the North Island, between Cook Strait and the Remutaka Range and it's the major population centre of the southern North Island",
+
+          // Hotels
+          'cordis_hotel': "Cordis, Auckland Hotel",
+          'cordis_brief':
+              "The Cordis Hotel, Auckland is an upscale-star hotel in Auckland, New Zealand. Formerly named The Langham, Auckland, it occupies the historic site of Partington's Windmill, a local landmark until its demolition in 1950.",
+          'hilton_hotel': "Hitlon, Aukcland Hotel",
+          'hilton_brief':
+              "Hilton Hotels & Resorts is a global brand of full-service hotels and resorts and the flagship brand of American multinational hospitality company Hilton.",
+          'novotel_hotel': "Novotel Hotel",
+          'novotel_brief':
+              "Novotel is a midscale hotel brand owned by Accor. Created in 1967 in France, the company grew into what became the Accor group in 1983, and Novotel remained a pillar brand of Accor's multi-brand strategy. Novotel manages 492 hotels in 59 countries.",
+          'sofitel_hotel': "Sofitel Hotel",
+          'sofitel_brief':
+              "Sofitel Hotels & Resorts are a chain of luxury hotels based in Paris, France, and owned by Accor since 1980.",
+
+          // Quoters
+          'evangline_quote':
+              "My favorite parts about 'The Battle of Five Armies' were the moments where you could clearly see that we were looking at New Zealand. That it wasn't done in post, it wasn't CGI, it was the beautiful, incredible creation of Mother Nature in all of her splendor.",
+          'evans_quote': "I feel that New Zealand is my second home."
         },
       };
 }
