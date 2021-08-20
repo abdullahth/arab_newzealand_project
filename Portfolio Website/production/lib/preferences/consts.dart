5d76ddd16676a1e58903008af03a7540ac5cc0e6
abdullahth <abdullahthurwatx@gmail.com>
2021-08-20 14:01:50 +0200

    Quotes Added

--- Portfolio Website/production/lib/preferences/consts.dart
+++ Portfolio Website/production/lib/preferences/consts.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:production/preferences/enums/assets.dart';
 import 'package:get/get.dart';
 
 final lightTheme = ThemeData(
@@ -12,3 +13,17 @@ final darkTheme = ThemeData(
   primaryColor: Color(0xFF30f05a),
   accentColor: Color(0xFFffffff),
 );
+
+final List<Map> quotes = [
+  {
+    'quoter': "Evangline Lilly",
+    'quote':
+        "evangline_quote".tr,
+    'asset': Assets.quoterEvanglineLilly.provider
+  },
+  {
+    'quoter': "Luke Evans",
+    'quote': "evans_quote".tr,
+    'asset': Assets.quoterLukeEvans.provider
+  }
+];
