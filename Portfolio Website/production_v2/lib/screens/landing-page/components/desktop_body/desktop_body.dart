d2f6ade5929f72e9ed91308f87ef9a4415115cf1
abdullahth <abdullahthurwatx@gmail.com>
2021-08-24 23:45:29 +0200

    Desktop Body Init

---
+++ Portfolio Website/production_v2/lib/screens/landing-page/components/desktop_body/desktop_body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LandingPageDesktopBody extends StatefulWidget {
+
+  @override
+  _LandingPageDesktopBodyState createState() => _LandingPageDesktopBodyState();
+}
+
+class _LandingPageDesktopBodyState extends State<LandingPageDesktopBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
