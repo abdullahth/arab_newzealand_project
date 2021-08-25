794d1fcac617eef4eb72c49b5520c128be4127fb
abdullahth <abdullahthurwatx@gmail.com>
2021-08-25 08:38:34 +0200

    Landing Page App Bar Placeholder

---
+++ Portfolio Website/production_v2/lib/screens/landing-page/components/app_bar/components/mobile_body/mobile_body.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LandingPageMobileAppBar extends StatefulWidget {
+  @override
+  _LandingPageMobileAppBarState createState() =>
+      _LandingPageMobileAppBarState();
+}
+
+class _LandingPageMobileAppBarState extends State<LandingPageMobileAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+     
+    );
+  }
+}
