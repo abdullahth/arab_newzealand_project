2fa8334c01516793549c3496fdfed46d4d7a145a
abdullahth <abdullahthurwatx@gmail.com>
2021-09-01 10:46:40 +0200

    Desktop Landing Page Body Localizations

--- Portfolio Website/production_v2/lib/prefernces/localizations.dart
+++ Portfolio Website/production_v2/lib/prefernces/localizations.dart
@@ -4,34 +4,157 @@ class AppLocalizations extends Translations {
   @override
   Map<String, Map<String, String>> get keys => {
         'en': {
+          'question_mark': "?",
+
           'aotara_group': "Aotara Group",
           'for_inv_and_dev': "For Investment and Development",
+          'about_com': "About our Company!",
+          'who_we_are': "Who we are",
+          'brief':
+              "A multicultural New Zealand group has expertise and specializations in all knowledge and practical sectors. We aim to transfer New Zealand experiences in all industries to the rest of the countries, emphasizing full respect for the privacy of nations and peoples and considering civilizational, cultural, and knowledge as an enriching factor to enhance and strengthen communication distinction and distance from division.",
+          'our_vision': "Our Vision",
+          'vision':
+              "To create a competent, advanced, and integrated work environment. For building, developing, and integrating diverse knowledge, scientific and varied sectors. Through the transfer of expertise between countries and the promotion of investment opportunities. Globally we are working with our partners in New Zealand and the Middle Eastern and African countries.",
+          'objective': "Objective",
+          'objective_breif':
+              "• Extending the concept of New Zealand's cultural and economic identity.\n• Building trust and exchanging real knowledge experiences in entrepreneurship between New Zealand and Arab and African countries.\n• Enhancing diversified investment opportunities between countries.\n• Enhancing infrastructure building and developing the leadership of diverse societies in the business sector.",
 
           // App Bar
           'about': "About",
-          'services': "Services",
+          'why_us': "Why Us",
           'testimonials': "Testimonials",
-          'contact': "Contact"
+          'contact': "Contact",
+          'industries': "Industries",
+
+          'why_us_long': "Why cooperating with Aotara Group is the best choice",
+
+          // Categories
+          'categories': "Categories",
+          'health': "Health",
+          'education': "Education",
+          'sport': "Sport",
+          'agriculture': "Agricluture",
+          'business': "Business",
+          'technology': "Technology",
+          'youth_development': "Youth Development",
+          'tourism': "Tourism",
+          'landscape': "Landscapes",
+          'woman': "Woman",
+
+          'phone_number': "Phone Number",
+          'email_address': "Email Address",
+
+          'experts': "Experts",
+          'our_team': "Our Team",
+          'human_power': "Human Power",
+          'experts_brief':
+              "Our experts knowing the culture and address customs-based communities.",
+          'our_team_brief':
+              "A diverse work team specialized in all knowledge and scientific sectors",
+          'human_power_brief':
+              "We believe that human power and time value are the most important ways to reach goals.",
         },
         'ar': {
+          'question_mark': "؟",
+
           'aotara_group': "مجموعة أوتارا",
           'for_inv_and_dev': "للإستثمار والتطوير",
+          'about_com': "حول شركتنا",
+          'who_we_are': "من نحن",
+          'brief':
+              "مجموعة نيوزيلاندية متعددة الثقافات لديها الخبرة والتخصصات في جميع القطاعات المعرفية والعملية. نهدف إلى نقل الخبرات النيوزيلندية في جميع الصناعات إلى بقية الدول ، والتأكيد على الاحترام الكامل لخصوصية الأمم والشعوب ، والإعتبار الحضاري والثقافي والمعرفي كعاملاً مؤثرًا لتعزيز وتقوية الاتصال والبعد عن الانقسام.",
+          'our_vision': "رؤيتنا",
+          'vision':
+              "هو خلق بيئة عمل كفؤة، متقدمة ومتكاملة؛ لبناء وتطوير ودمج المعرفة والقطاعات العلمية المتنوعة من خلال نقل الخبرات بين الدول وتعزيز فرص الاستثمار.\n على الصعيد العالمي ، نحن نعمل مع شركائنا في نيوزيلندا ودول الشرق الأوسط وأفريقيا.",
+          'objective': "الأهداف",
+          'objective_breif':
+              "• توسيع مفهوم الهوية الثقافية والاقتصادية لنيوزيلندا. \n • بناء الثقة وتبادل الخبرات المعرفية الحقيقية في ريادة الأعمال بين نيوزيلندا والدول العربية والأفريقية. \n • تعزيز فرص الاستثمار المتنوعة بين البلدان. \n • تعزيز بناء البنية التحتية و تطوير قيادة المجتمعات المتنوعة في قطاع الأعمال.",
 
           // App Bar
           'about': "حول",
-          'services': "الخدمات",
+          'why_us': "لماذا نحن",
           'testimonials': "التوصيات",
-          'contact': "تواصل"
+          'contact': "تواصل",
+          'industries': "المجالات",
+
+          'why_us_long': "لماذا يعتبر التعاون مع مجموعة أوتارا هو الخيار الأفضل",
+
+          // Categories
+          'categories': "التصنيفات",
+          'health': "الصحة",
+          'education': "التعليم",
+          'sport': "الرياضة",
+          'agriculture': "الزراعة",
+          'business': "الأعمال",
+          'technology': "التكنولوجيا",
+          'youth_development': "تنمية الشباب",
+          'tourism': "السياحة",
+          'landscape': "المعالم الطبيعية",
+          'woman': "المرأة",
+
+          'phone_number': "رقم الهاتف",
+          'email_address': "البريد الإلكتروني",
+
+          'experts': "خبرائنا",
+          'our_team': "فريقنا",
+          'human_power': "القوة البشرية",
+          'experts_brief':
+              "خبرائنا يعرفون الثقافة ويتعاملون مع المجتمعات القائمة على الجمارك.",
+          'our_team_brief':
+              "فريق عمل متنوع متخصص في جميع القطاعات المعرفية والعلمية",
+          'human_power_brief':
+              "نحن نؤمن بأن القوة البشرية وقيمة الوقت هي أهم الطرق للوصول إلى الأهداف.",
         },
         'fr': {
+          'question_mark': "?",
+
           'aotara_group': "Groupe Aotara",
           'for_inv_and_dev': "Pour l'investissement et le développement",
+          'about_com': "Au sujet de notre compagnie",
+          'who_we_are': "Qui nous sommes",
+          'brief':
+              "Un groupe néo-zélandais multiculturel possède une expertise et des spécialisations dans tous les secteurs de la connaissance et de la pratique. Nous visons à transférer les expériences néo-zélandaises dans toutes les industries au reste des pays, en mettant l'accent sur le plein respect de la vie privée des nations et des peuples et en considérant la civilisation, la culture et les connaissances comme un facteur d'enrichissement pour améliorer et renforcer la distinction de communication et la distance par rapport à la division.",
+          'our_vision': "Notre Vision",
+          'vision':
+              "est de créer un environnement de travail compétent, avancé et intégré. Pour construire, développer et intégrer des connaissances diverses, des secteurs scientifiques et variés. Par le transfert d'expertise entre les pays et la promotion des opportunités d'investissement. À l'échelle mondiale, nous travaillons avec nos partenaires en Nouvelle-Zélande et dans les pays du Moyen-Orient et d'Afrique.",
+          'objective': "Objectif",
+          'objective_breif':
+              "• Élargir le concept d'identité culturelle et économique de la Nouvelle-Zélande.\n• Construire la confiance et échanger de véritables expériences de connaissances en matière d'entrepreneuriat entre la Nouvelle-Zélande et les pays arabes et africains.\n• Améliorer les opportunités d'investissement diversifiées entre les pays.\n• Améliorer la construction d'infrastructures et développer le leadership de sociétés diversifiées dans le secteur des affaires.",
 
           // App Bar
           'about': "À propos",
-          'services': "Services",
+          'why_us': "Pourquoi nous",
           'testimonials': "Témoignages",
           'contact': "Contact",
+          'industries': "les industries",
+
+          'why_us_long': "Pourquoi coopérer avec Aotara Group est le meilleur choix",
+
+          // Categories
+          'categories': "Catégories",
+          'health': "La santé",
+          'education': "Éducation",
+          'sport': "Des sports",
+          'agriculture': "L' agriculture",
+          'business': "L' entreprise",
+          'technology': "La technologie",
+          'youth_development': "Développement de la jeunesse",
+          'tourism': "Tourisme",
+          'landscape': "Paysages",
+          'woman': "Femme",
+
+          'phone_number': "Téléphone",
+          'email_address': "Adresse e-mail",
+
+          'experts': "Les experts",
+          'our_team': "Notre équipe",
+          'human_power': "La puissance humaine",
+          'experts_brief':
+              "Nos experts connaissent la culture et s'adressent aux communautés coutumières.",
+          'our_team_brief':
+              "Une équipe de travail diversifiée et spécialisée dans tous les secteurs du savoir et de la science",
+          'human_power_brief':
+              "Nous croyons que la puissance humaine et la valeur du temps sont les moyens les plus importants pour atteindre les objectifs.",
         },
       };
 }
