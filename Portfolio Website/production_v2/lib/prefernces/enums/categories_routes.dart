eaf7067c0c586711d8e2ebf5494a127cc0c89add
abdullahth <abdullahthurwatx@gmail.com>
2021-09-01 10:47:40 +0200

    Categories Routes

---
+++ Portfolio Website/production_v2/lib/prefernces/enums/categories_routes.dart
@@ -0,0 +1,54 @@
+enum CategoriesRoutes {
+  anonymosCategoryName,
+  sports,
+  agriculture,
+  education,
+  youthDevelopment,
+  health,
+  technology,
+  business,
+  tourism,
+  landscape, 
+  women,
+}
+
+extension ext on CategoriesRoutes {
+  Function(String categoryName)? get navigateByCategoryNameCallback {
+    switch (this) {
+      case CategoriesRoutes.anonymosCategoryName:
+        return (categoryName) {
+          print(categoryName);
+        };
+
+      default:
+        return null;
+    }
+  }
+
+  String? get routeUrl {
+    switch (this) {
+      case CategoriesRoutes.agriculture:
+        return 'agriculture';
+      case CategoriesRoutes.sports:
+        return 'sports';
+      case CategoriesRoutes.education:
+        return 'education';
+      case CategoriesRoutes.youthDevelopment:
+        return 'youthDevelopment';
+      case CategoriesRoutes.health:
+        return 'health';
+      case CategoriesRoutes.technology:
+        return 'technology';
+      case CategoriesRoutes.business:
+        return 'business';
+      case CategoriesRoutes.tourism:
+        return 'tourism';
+      case CategoriesRoutes.landscape:
+        return 'landscape';
+      case CategoriesRoutes.women:
+        return 'women';
+      default:
+        return null;
+    }
+  }
+}
